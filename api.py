67af1360246806a6fc05133ebd79fae533c5c5b3
git-clone-abhinav <abhi.anu003@gmail.com>
2022-04-24 13:05:36 +0530

    other API endpoints

--- api.py
+++ api.py
@@ -10,16 +10,37 @@ CORS(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-@app.route('/', methods = ['GET'])
-def root():
+
+@app.route('/', methods=['GET'])
+def home():
+    logger.logit("Rendered home page")
+    return 'Powered by <a href="https://codingclub.pichavaram.in" target=_blank>Coding Club</a>'
+
+@app.route('/users', methods = ['GET'])
+def users():
+    # localhost:5000/users
     query = f'SELECT * from users ;'
     result = queries.runQuery(query,data=None,queryType="non-DQL")
     if len(result) != 0:
-        response = {'headers':['User ID','Name',"Check Point 1 TimeStamp","Finish TimeStamp","Rank"],'rows':result}
+        response = {'headers':['User ID','Name',"Group","Check Point 1 TimeStamp","Finish TimeStamp","Rank"],'rows':result}
     else:
         response = {'headers':["No Data"],'rows':[]}
-    logger.logit("Rendered `ROOT`")
-    return jsonify(response)
+    logger.logit("Rendered `users`")
+    return response
+
+@app.route('/config', methods = ['GET'])
+def config():
+    # localhost:5000/config
+    query = f'SELECT * from config ;'
+    result = queries.runQuery(query,data=None,queryType="non-DQL")
+    if len(result) != 0:
+        response = {'headers':["Last Rank"],'rows':result[0][0]}
+    else:
+        response = {'headers':["No Data"],'rows':[]}
+    logger.logit("Rendered `config`")
+    return response
+
+
 
 @app.route('/insert_user', methods = ['GET'])
 def insertUser():
@@ -34,9 +55,92 @@ def insertUser():
     finally:
         return jsonify(response)
 
+
+@app.route('/getrunners',methods=['GET'])
+def get_runners():
+    # localhost:2400/getrunners?group_id=A
+    group_id = str(request.args.get('group_id'))
+    try:
+        result = queries.runQuery(f"SELECT * FROM users WHERE group_id='{group_id}'",None,"non-DML")
+        response = {'headers':["Success"],'rows':result}
+        logger.logit(f"Rendered `get_runners` for group {group_id}")
+    except Exception as e:
+        response = {'headers':["Error"],'rows':[e]}
+        logger.logit(f"Error while inserting user: {e}")
+    finally:
+        return jsonify(response)
+
+
+@app.route('/getrunnerinfo',methods=['GET'])
+def get_runner_info():
+    # localhost:2400/getrunnerinfo?user_id=21f1002369
+    user_id = str(request.args.get('user_id'))
+    try:
+        result = queries.runQuery(f"SELECT * FROM users WHERE uid='{user_id}'",None,"non-DML")
+        if len(result)==0:
+            response = {'headers':["Error"],'rows':["No Data for this user"]}
+        else:
+            inserter = []
+            inserter.append(result[0][0])
+            inserter.append(result[0][1])
+            inserter.append(result[0][2])
+            if result[0][3]==None:
+                inserter.append("Not yet arrived ")
+            else:
+                inserter.append(result[0][3])
+            if result[0][4]==None:
+                inserter.append("Not yet arrived")
+            else:
+                inserter.append(result[0][4])
+            if result[0][5]==None:
+                inserter.append("Not assigned yet")
+            else:
+                inserter.append(result[0][5])
+            response = {'headers':['User ID','Name',"Group","Check Point 1 TimeStamp","Finish TimeStamp","Rank"],'rows':inserter}
+        logger.logit(f"Rendered `get_runner_info` for user {user_id}")
+    except Exception as e:
+        response = {'headers':["Error"],'rows':[e]}
+        logger.logit(f"Error while inserting user: {e}")
+    finally:
+        return response
+
+@app.route('/updatecheckpoint',methods=['GET'])
+def update_checkpoint():
+    # localhost:2400/updatecheckpoint?user_id=21f1002369
+    user_id = str(request.args.get('user_id'))
+    try:
+        result = queries.runQuery(f"UPDATE users SET cp1='{logger.get_time()}' WHERE uid='{user_id}'",None,"DML")
+        response = {'headers':["Success"],'rows':result}
+        logger.logit(f"Updated checkpoint for user `{user_id}`")
+    except Exception as e:
+        response = {'headers':["Error"],'rows':[e]}
+        logger.logit(f"Error while inserting user: {e}")
+    finally:
+        return jsonify(response)
+
+@app.route('/updatefinish',methods=['GET'])
+def update_finish():
+    # localhost:2400/updatefinish?user_id=21f1002369
+    user_id = str(request.args.get('user_id'))
+    try:
+        # localhost:2400/updatefinish?user_id=21f1002369
+        rank = queries.runQuery(f"SELECT * FROM config",None,"non-DML")
+        rank = rank[0][0]
+        queries.runQuery(f"UPDATE config SET last_rank={rank+1}",None,"DML")
+        queries.runQuery(f"UPDATE users SET finish='{logger.get_time()}',rank={rank+1} WHERE uid='{user_id}'",None,"DML")
+        response = {'headers':["Success"],'rows':rank}
+        logger.logit(f"Updated finish for user `{user_id}` with rank {rank+1}")
+    except Exception as e:
+        response = {'headers':["Error"],'rows':[e]}
+        logger.logit(f"Error while inserting user: {e}")
+    finally:
+        return jsonify(response)
+
 @app.route('/upload')
 def page_upload():
+    # localhost:2400/upload
     return render_template('uploader.html')
+
 
 @app.route('/uploader', methods = ['GET','POST'])
 def upload():
@@ -53,14 +157,28 @@ def upload():
             csvreader = csv.reader(csvfile)
             val = []
             for row in csvreader:
-                val.append((row[0],row[1]))
+                val.append((row[0],row[1],row[2]))
         try:
-            queries.runQuery("INSERT INTO users (uid,name) VALUES (%s,%s)",val,"many")
+            queries.runQuery("INSERT INTO users (uid,name,group_id) VALUES (%s,%s,%s)",val,"many")
             response = {'headers':["Upload Succesfull"],'rows':val}
             logger.logit(f"Updated Database `{val}`")
         except Exception as e:
             response = {'headers':["Error while uploading to database"],'rows':[e]}
             logger.logit(f"Error while uploading to database: {e}")
+        return response
+
+@app.route('/delete', methods = ['GET'])
+def deleteUserData():
+    # localhost:2400/delete
+    try:
+        queries.runQuery("DELETE FROM users;",None,"DML")
+        queries.runQuery(f"UPDATE config SET last_rank=0",None,"DML")
+        response = {'headers':["Success"],'rows':'Deleted'}
+        logger.logit("Deleted all data from `users`&`config` table")
+    except Exception as e:
+        response = {'headers':["Error"],'rows':[e]}
+        logger.logit(f"Error while deleting user: {e}")
+    finally:
         return response
 
 if __name__ == "__main__":
